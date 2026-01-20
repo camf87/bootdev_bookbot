b17f6e50555a12c63a025db99e1e4e45448cc5b9
calex <cfernandes87@gmail.com>
2026-01-20 13:02:26 +0000

    finish bookbot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words, count_char
+from stats import get_num_words, count_char, sort_dict
+import sys
 
 def get_book_text(path):
     with open(path) as f:
@@ -7,9 +8,22 @@ def get_book_text(path):
 
 
 def main():
-    content=get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+
+    file=sys.argv[1]
+    content=get_book_text(file)
     count= get_num_words(content)
-    print(f'Found {count} total words')
-    print(count_char(content))
+    # print(f'Found {count} total words')
+    # print(count_char(content))
+    dictionary = count_char(content)
+    print(f'''=========== BOOKBOT ============
+Analyzing book found at {file}...
+----------- Word Count ----------
+Found {count} total words
+--------- Character Count -------''')
+    for item in sort_dict(dictionary):
+        print(f"{item['char']}: {item['num']}")
 
 main()
--- stats.py
+++ stats.py
@@ -9,3 +9,13 @@ def count_char(text):
         else:
             dict[c]=1
     return dict
+
+def sort_on(items, key):
+    return items[key]
+
+def sort_dict(dictionary):
+    dict_sorted = sorted(dictionary.items(), key=lambda item: item[1], reverse=True)
+    res = []
+    for k,v in dict_sorted:
+        res.append({"char": k, "num": v})
+    return res
